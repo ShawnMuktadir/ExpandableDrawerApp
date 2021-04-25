e253530a4db5b17464b4268325b43a19c42e72c5
ShawnMuktadir <shawn.muktadir03@gmail.com>
2021-04-25 18:13:44 +0600

    edit-profile: Create edit profile UI

--- app/src/main/java/www/fiberathome/com/parkingapp/ui/profile/edit/EditProfileFragment.java
+++ app/src/main/java/www/fiberathome/com/parkingapp/ui/profile/edit/EditProfileFragment.java
@@ -1,30 +1,30 @@
-package www.fiberathome.com.parkingapp.ui.profile;
+package www.fiberathome.com.parkingapp.ui.profile.edit;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.textfield.TextInputLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import de.hdodenhof.circleimageview.CircleImageView;
 import timber.log.Timber;
 import www.fiberathome.com.parkingapp.R;
 import www.fiberathome.com.parkingapp.model.api.AppConfig;
 import www.fiberathome.com.parkingapp.model.data.preference.Preferences;
 import www.fiberathome.com.parkingapp.model.user.User;
 import www.fiberathome.com.parkingapp.ui.home.HomeFragment;
-import www.fiberathome.com.parkingapp.ui.profile.edit.EditProfileActivity;
 import www.fiberathome.com.parkingapp.utils.IOnBackPressListener;
 import www.fiberathome.com.parkingapp.utils.TastyToastUtils;
 import www.fiberathome.com.parkingapp.utils.TextUtils;
@@ -33,33 +33,42 @@ import static android.content.Context.LOCATION_SERVICE;
 
 @SuppressLint("NonConstantResourceId")
 @SuppressWarnings("unused")
-public class ProfileFragment extends Fragment implements IOnBackPressListener {
+public class EditProfileFragment extends Fragment implements IOnBackPressListener {
 
-    @BindView(R.id.tvUserName)
-    TextView tvUserName;
+    @BindView(R.id.textInputLayoutFullName)
+    TextInputLayout textInputLayoutFullName;
+
+    @BindView(R.id.editTextFullName)
+    EditText editTextFullName;
+
+    @BindView(R.id.textInputLayoutCarNumber)
+    TextInputLayout textInputLayoutCarNumber;
+
+    @BindView(R.id.editTextCarNumber)
+    EditText editTextCarNumber;
 
     @BindView(R.id.tvUserMobileNo)
     TextView tvUserMobileNo;
 
-    @BindView(R.id.tvUserVehicleNo)
-    TextView tvUserVehicleNo;
+    @BindView(R.id.imageViewEditProfileImage)
+    CircleImageView imageViewEditProfileImage;
 
-    @BindView(R.id.ivUserProfilePic)
-    ImageView ivUserProfilePic;
+    @BindView(R.id.imageViewCaptureImage)
+    ImageView imageViewCaptureImage;
 
-    @BindView(R.id.btn_update_info)
-    Button btnUpdateInfo;
+   /* @BindView(R.id.btn_update_info)
+    Button btnUpdateInfo;*/
 
     private Unbinder unbinder;
 
-    private ProfileActivity context;
+    private EditProfileActivity context;
 
-    public ProfileFragment() {
+    public EditProfileFragment() {
         // Required empty public constructor
     }
 
-    public static ProfileFragment newInstance() {
-        return new ProfileFragment();
+    public static EditProfileFragment newInstance() {
+        return new EditProfileFragment();
     }
 
     @Override
@@ -72,11 +81,11 @@ public class ProfileFragment extends Fragment implements IOnBackPressListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_new, container, false);
 
         unbinder = ButterKnife.bind(this, view);
 
-        context = (ProfileActivity) getActivity();
+        context = (EditProfileActivity) getActivity();
 
         User user = Preferences.getInstance(context).getUser();
 
@@ -88,7 +97,9 @@ public class ProfileFragment extends Fragment implements IOnBackPressListener {
     }
 
     private void setListeners() {
-        btnUpdateInfo.setOnClickListener(v -> startActivity(new Intent(context, EditProfileActivity.class)));
+        /*btnUpdateInfo.setOnClickListener(v-> {
+
+        });*/
     }
 
     @Override
@@ -120,18 +131,17 @@ public class ProfileFragment extends Fragment implements IOnBackPressListener {
         }
 
         String name = user.getFullName();
-        //name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
         name = TextUtils.getInstance().capitalizeFirstLetter(name);
-        tvUserName.setText(name);
+        editTextFullName.setText(name);
 
         tvUserMobileNo.setText(TextUtils.getInstance().addCountryPrefix(user.getMobileNo()));
         Timber.e("Mobile no -> %s", user.getMobileNo());
 
-        tvUserVehicleNo.setText(user.getVehicleNo());
+        editTextCarNumber.setText(user.getVehicleNo());
 
         String url = AppConfig.IMAGES_URL + user.getImage() + ".jpg";
         Timber.e("Image URL -> %s", url);
-        Glide.with(context).load(url).placeholder(R.drawable.blank_profile).dontAnimate().into(ivUserProfilePic);
+        Glide.with(context).load(url).placeholder(R.drawable.blank_profile).dontAnimate().into(imageViewEditProfileImage);
     }
 
     private boolean isGPSEnabled() {
