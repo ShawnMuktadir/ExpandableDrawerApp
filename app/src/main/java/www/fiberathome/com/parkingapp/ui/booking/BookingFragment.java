6c808105e7599f010f5e722c6437d65fc6e88e24
abdurdp <abdur.desertphenix@gmail.com>
2021-11-28 16:00:00 +0600

    parking-7 :  refactor code

--- app/src/main/java/www/fiberathome/com/parkingapp/ui/booking/BookingFragment.java
+++ app/src/main/java/www/fiberathome/com/parkingapp/ui/booking/BookingFragment.java
@@ -233,8 +233,8 @@ public class BookingFragment extends BaseFragment implements IOnBackPressListene
                         DialogUtils.getInstance().showMessageDialog(context.getResources().getString(R.string.already_booked_msg), context);
                     } else {
                         count = "25";
-                        if (placeId != null && !placeId.equalsIgnoreCase("") && placeId.equalsIgnoreCase("0") &&
-                                count != null && !count.equalsIgnoreCase("") && count.equalsIgnoreCase("0")) {
+                        if (placeId != null && !placeId.equalsIgnoreCase("") && !placeId.equalsIgnoreCase("0") &&
+                                count != null && !count.equalsIgnoreCase("") && !count.equalsIgnoreCase("0")) {
                             Bundle bundle = new Bundle();
                             bundle.putBoolean("m", false); //m for more
                             bundle.putString("areaPlacedId", placeId);
